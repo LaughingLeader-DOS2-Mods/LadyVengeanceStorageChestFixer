ec2a50b6a2e219e33b6edd006a826404bb23e889
LaughingLeader <laughingleader@gmail.com>
2020-01-26 14:13:20 -0600

    Added a script to fix broken top deck items (the stairs)

--- Mods/PlayerChestFixer_947ff25c-956b-400b-88aa-379e0a72411d/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/PlayerChestFixer_947ff25c-956b-400b-88aa-379e0a72411d/Story/RawFiles/Lua/Bootstrap.lua
@@ -1,6 +1,11 @@
 
-function LLPLAYERCHEST_Ext_LogPosition(level, obj, pitch, yaw, roll)
+function LLPLAYERCHEST_Ext_LogPosition(prefix, level, obj)
 	local x,y,z = GetPosition(obj)
+	local pitchdegree,yawdegree,rolldegree = GetRotation(obj)
+
+	local pitch = RealProduct(pitchdegree, 0.0174533)
+	local yaw = RealProduct(yawdegree, 0.0174533)
+	local roll = RealProduct(rolldegree, 0.0174533)
 	--Ext.Print(tostring(obj) .. " | Position: x("..tostring(x)..")y("..tostring(y)..")z("..tostring(z)..") | Rotation: pitch("..tostring(pitch)..") yaw("..tostring(yaw)..") roll("..tostring(roll)..")")
-	Ext.Print("DB_LLPLAYERCHEST_FallbackPosition(''"..level.."'', "..tostring(obj) .. ", "..tostring(x)..", "..tostring(y)..", "..tostring(z)..", "..tostring(pitch)..", "..tostring(yaw)..", "..tostring(roll)..");")
+	Ext.Print(prefix .. "(\""..level.."\", "..tostring(obj) .. ", "..tostring(x)..", "..tostring(y)..", "..tostring(z)..", "..tostring(pitch)..", "..tostring(yaw)..", "..tostring(roll)..");")
 end
